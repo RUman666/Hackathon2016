9516f9c3d204f2ab7bb78a0dfeba34e1cefb1cf2
akazi1421 <akazi1421@CHHWB11C6DCEEE.inst.hcpss.org>
2016-04-16 11:09:04 -0400

    s

--- Hackathon/src/Junky.java
+++ Hackathon/src/Junky.java
@@ -1,6 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-public class Junky 
-{
-.
+public class Junky extends JFrame {
+
+    public Junky() {
+       setTitle("Simple example");
+       setSize(300, 200);
+       setLocationRelativeTo(null);
+       setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+       Junky ex = new Junky();
+       ex.setVisible(true);
+    }
 }
-b
